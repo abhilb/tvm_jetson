7e325c53ea463736e3abdbb65fdd525397e4d7b6
Abhilash <abhilashbabuj@gmail.com>
2022-01-29 04:47:57 +0100

    Updated dockerfile and build script

--- build.sh
+++ build.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
+
+echo "======================================="
 echo "Build the latest version of Apache TVM"
+echo "======================================="
+
 git clone --recursive https://github.com/apache/tvm.git
-cd tvm
+
+echo "+++++++++++++ Completed cloning apache tvm ++++++++++++++++"
+
+ls -l
+
+pushd tvm
 mkdir -p build
-cp config.cmake tvm/build
-cd /tvm/build
+cp /config.cmake tvm/build
+pushd build
 cmake ..
 make -j
+popd
+
+echo "======================================="
+echo "Build python packages"
+echo "======================================="
+pushd python
+python3 setup.py bdist_wheel
+popd
+
+echo "======================================="
+echo "Done"
+echo "======================================="
